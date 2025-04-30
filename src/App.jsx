d96d442a4e02d97e42ef4e935b46545759bd6ba1
trishannehui <huidemt@gmail.com>
2025-04-30 10:33:35 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BookSearch from "./components/BookSearch";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1 className="app-title"> </h1>
+      <BookSearch />
+    </div>
+  );
+}
+
+export default App;
